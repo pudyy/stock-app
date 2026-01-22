972e3fd6d1fa0db5ed9b5f5ba537a155545662e6
Pedro Augusto <contato.puddy@gmail.com>
2026-01-22 11:11:13 -0300

    switch prisma adapter to postgres

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,7 @@
-// src/lib/prisma.ts
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../../generated/prisma/client";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma"; // ajuste o caminho conforme seu projeto [web:433]
 
-const connectionString = process.env.DATABASE_URL ?? "file:./dev.db";
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient({ adapter });
