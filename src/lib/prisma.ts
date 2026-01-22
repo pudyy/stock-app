346ebefcc0426b30241df8853a6ef14df536cb2a
Pedro Augusto <contato.puddy@gmail.com>
2026-01-22 11:13:11 -0300

    switch prisma adapter to postgres

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma"; // ajuste o caminho conforme seu projeto [web:433]
+import { PrismaClient } from "../../generated/prisma/client"; // ajuste o caminho conforme seu projeto [web:433]
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 
